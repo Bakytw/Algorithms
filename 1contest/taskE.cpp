62c7a869d8e116088b5c9fcdb1ff40f591e697ff
Bakytw <89724301+Bakytw@users.noreply.github.com>
2022-10-24 19:22:39 +0300

    Update taskE.cpp

--- 1contest/taskE.cpp
+++ 1contest/taskE.cpp
@@ -15,45 +15,11 @@ class Stack {
  public:
   size_t Size() { return stacksize_; }
   int Back() { return top_->value; }
-
-  void Push(int elem) {
-    if (top_ == nullptr) {
-      top_ = new Node(elem);
-      top_->min = elem;
-    } else {
-      top_->next = new Node(elem);
-      top_->next->prev = top_;
-      top_ = top_->next;
-      top_->min = std::min(elem, top_->prev->min);
-    }
-    ++stacksize_;
-  }
-
+  void Push(int elem);
   bool Empty() { return stacksize_ == 0u; }
-
-  void Pop() {
-    if (Empty()) {
-      return;
-    }
-    if (stacksize_ == 1) {
-      delete top_;
-      top_ = nullptr;
-    } else {
-      top_ = top_->prev;
-      delete top_->next;
-      top_->next = nullptr;
-    }
-    --stacksize_;
-  }
-
+  int Pop();
   int Min() { return top_->min; }
-
-  void Clear() {
-    while (!Empty()) {
-      Pop();
-    }
-  }
-
+  void Clear();
   ~Stack() { Clear(); }
 };
 
@@ -64,70 +30,21 @@ class Queue {
 
  public:
   size_t Size() { return queuesize_; }
-
   bool Empty() { return queuesize_ == 0u; }
-  void Shifting() {
-    if (Empty()) {
-      std::cout << "error" << std::endl;
-      return;
-    }
-    if (s2_.Empty()) {
-      while (!s1_.Empty()) {
-        s2_.Push(s1_.Back());
-        s1_.Pop();
-      }
-    }
-  }
-
-  void Front() {
-    Shifting();
-    if (s2_.Empty()) {
-      return;
-    }
-    std::cout << s2_.Back() << std::endl;
-  }
-
-  void Enqueue(int elem) {
-    s1_.Push(elem);
-    ++queuesize_;
-  }
-
-  void Dequeue() {
-    if (Empty()) {
-      std::cout << "error" << std::endl;
-      return;
-    }
-    Front();
-    s2_.Pop();
-    --queuesize_;
-  }
-
-  void Min() {
-    int ans = 0;
-    if (Empty()) {
-      std::cout << "error" << std::endl;
-      return;
-    }
-    if (s1_.Empty() || s2_.Empty()) {
-      ans = s1_.Empty() ? s2_.Min() : s1_.Min();
-    } else {
-      ans = std::min(s2_.Min(), s1_.Min());
-    }
-    std::cout << ans << std::endl;
-  }
-
-  void Clear() {
-    s1_.Clear();
-    s2_.Clear();
-    queuesize_ = 0;
-  }
-
+  int Shifting();
+  int Front();
+  void Enqueue(int elem);
+  int Dequeue();
+  int Min();
+  void Clear();
   ~Queue() { Clear(); }
 };
 
+void CheckErrors(int temp);
+
 int main() {
   Queue q;
-  int elem, n;
+  int elem, n, answer;
   std::cin >> n;
   std::string word;
   for (int i = 0; i < n; ++i) {
@@ -137,17 +54,115 @@ int main() {
       q.Enqueue(elem);
       std::cout << "ok" << std::endl;
     } else if (word == "dequeue") {
-      q.Dequeue();
+      answer = q.Dequeue();
+      CheckErrors(answer);
     } else if (word == "front") {
-      q.Front();
+      answer = q.Front();
+      CheckErrors(answer);
     } else if (word == "size") {
       std::cout << q.Size() << std::endl;
     } else if (word == "clear") {
       q.Clear();
       std::cout << "ok" << std::endl;
     } else if (word == "min") {
-      q.Min();
+      answer = q.Min();
+      CheckErrors(answer);
     }
   }
   return 0;
+}
+
+void Stack::Push(int elem) {
+  if (top_ == nullptr) {
+    top_ = new Node(elem);
+    top_->min = elem;
+  } else {
+    top_->next = new Node(elem);
+    top_->next->prev = top_;
+    top_ = top_->next;
+    top_->min = std::min(elem, top_->prev->min);
+  }
+  ++stacksize_;
+}
+
+int Stack::Pop() {
+  if (Empty()) {
+    return -1;
+  }
+  int temp = top_->value;
+  if (stacksize_ == 1) {
+    delete top_;
+    top_ = nullptr;
+  } else {
+    top_ = top_->prev;
+    delete top_->next;
+    top_->next = nullptr;
+  }
+  --stacksize_;
+  return temp;
+}
+
+void Stack::Clear() {
+  while (!Empty()) {
+    Pop();
+  }
+}
+
+int Queue::Shifting() {
+  if (Empty()) {
+    return -1;
+  }
+  if (s2_.Empty()) {
+    while (!s1_.Empty()) {
+      s2_.Push(s1_.Back());
+      s1_.Pop();
+    }
+  }
+  return 0;
+}
+
+int Queue::Front() {
+  Shifting();
+  if (s2_.Empty()) {
+    return -1;
+  }
+  return s2_.Back();
+}
+
+void Queue::Enqueue(int elem) {
+  s1_.Push(elem);
+  ++queuesize_;
+}
+
+int Queue::Dequeue() {
+  if (Empty()) {
+    return -1;
+  }
+  Front();
+  --queuesize_;
+  return s2_.Pop();
+}
+
+int Queue::Min() {
+  if (Empty()) {
+    return -1;
+  }
+  if (s1_.Empty() || s2_.Empty()) {
+    return s1_.Empty() ? s2_.Min() : s1_.Min();
+  }
+  return std::min(s2_.Min(), s1_.Min());
+}
+
+void Queue::Clear() {
+  s1_.Clear();
+  s2_.Clear();
+  queuesize_ = 0;
+}
+
+void CheckErrors(int temp) {
+  if (temp == -1) {
+    std::cout << "error" << '\n';
+  } else {
+    std::cout << temp << '\n';
+  }
 }
