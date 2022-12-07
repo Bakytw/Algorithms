bc57d3a123a15a790e99cb390dd999a2d9b41a82
Bakytw <89724301+Bakytw@users.noreply.github.com>
2022-12-07 03:21:07 +0300

    Update taskH.cpp

--- 3contest/taskH.cpp
+++ 3contest/taskH.cpp
@@ -2,17 +2,24 @@
 #include <iostream>
 #include <vector>
 
-std::pair<long long, long long> MinPair(std::pair<long long, long long> one,
-                                        std::pair<long long, long long> two);
+using Ll = long long;
+
+struct PairsOfMins {
+  Ll first_stat, second_stat;
+  PairsOfMins(Ll first_stat, Ll second_stat)
+      : first_stat(first_stat), second_stat(second_stat) {}
+};
+
+PairsOfMins MinPair(PairsOfMins one, PairsOfMins two);
 
 class SparseTable {
  private:
   std::vector<size_t> log_;
-  std::vector<std::vector<std::pair<long long, long long>>> sparse_;
+  std::vector<std::vector<PairsOfMins>> sparse_;
 
  public:
-  explicit SparseTable(const std::vector<long long>& a);
-  long long Get(size_t l, size_t r);
+  explicit SparseTable(const std::vector<Ll>& a);
+  Ll Get(size_t l, size_t r);
 };
 
 int main() {
@@ -21,35 +28,35 @@ int main() {
   std::cout.tie(nullptr);
   size_t n, m;
   std::cin >> n >> m;
-  std::vector<long long> sequence(n);
+  std::vector<Ll> sequence(n);
   for (size_t i = 0; i < n; ++i) {
     std::cin >> sequence[i];
   }
   SparseTable st(sequence);
   for (size_t i = 0; i < m; ++i) {
-    long long l, r;
+    Ll l, r;
     std::cin >> l >> r;
     std::cout << st.Get(l - 1, r - 1) << '\n';
   }
   return 0;
 }
 
-std::pair<long long, long long> MinPair(std::pair<long long, long long> one,
-                                        std::pair<long long, long long> two) {
-  std::vector<long long> temp{one.first, one.second, two.first, two.second};
+PairsOfMins MinPair(PairsOfMins one, PairsOfMins two) {
+  std::vector<Ll> temp{one.first_stat, one.second_stat, two.first_stat,
+                       two.second_stat};
   std::sort(temp.begin(), temp.end());
   temp.resize(std::unique(temp.begin(), temp.end()) - temp.begin());
   if (temp.size() > 1) {
     for (size_t i = 0; i < temp.size(); ++i) {
       if (temp[i] != temp[0]) {
-        return std::make_pair(temp[0], temp[i]);
+        return {temp[0], temp[i]};
       }
     }
   }
-  return std::make_pair(temp[0], temp[0]);
+  return {temp[0], temp[0]};
 }
 
-SparseTable::SparseTable(const std::vector<long long>& a) {
+SparseTable::SparseTable(const std::vector<Ll>& a) {
   size_t n = a.size();
   log_.resize(n + 1);
   log_[1] = 0;
@@ -61,11 +68,10 @@ SparseTable::SparseTable(const std::vector<long long>& a) {
   }
   sparse_.resize(log_[n] + 1);
   for (size_t i = 0; i < log_[n] + 1; ++i) {
-    sparse_[i].resize(n);
+    sparse_[i].resize(n, {0, 0});
   }
   for (size_t i = 0; i < n; ++i) {
-    sparse_[0][i].first = a[i];
-    sparse_[0][i].second = a[i];
+    sparse_[0][i] = PairsOfMins(a[i], a[i]);
   }
   for (size_t k = 0; k < log_[n]; ++k) {
     for (size_t i = 0; i < n; ++i) {
@@ -77,7 +83,7 @@ SparseTable::SparseTable(const std::vector<long long>& a) {
   }
 }
 
-long long SparseTable::Get(size_t l, size_t r) {
+Ll SparseTable::Get(size_t l, size_t r) {
   size_t k = log_[r - l + 1];
-  return MinPair(sparse_[k][l], sparse_[k][r - (1 << k) + 1]).second;
+  return MinPair(sparse_[k][l], sparse_[k][r - (1 << k) + 1]).second_stat;
 }
