899c6ea1ce1ed8de5e19dd656a0ba4b43295f8ae
Bakytw <89724301+Bakytw@users.noreply.github.com>
2023-03-13 00:06:25 +0300

    Update taskN.cpp

--- 6contest/taskN.cpp
+++ 6contest/taskN.cpp
@@ -1,24 +1,41 @@
 /*
  * dp[i][j][h] - минимальное расстояние редактирования между подстроками s[1:i]
- * и t[1:j] при условии, что было сделано не более h операций
+ * и t[1:j] при условии, что было сделано не более h операций?
  */
 #include <iostream>
 #include <vector>
 
 const long long kInf = 1e18;
 
+void DpBuild(std::vector<std::vector<std::vector<long long>>>& dp,
+             const std::string& s, const std::string& t, long long k);
 int main() {
   std::string s, t;
   long long k;
   std::cin >> s >> t;
   std::cin >> k;
-  auto n = static_cast<long long>(s.size()),
-       m = static_cast<long long>(t.size());
   s.insert(s.begin(), '$');
   t.insert(t.begin(), '$');
   std::vector<std::vector<std::vector<long long>>> dp(
-      n + 1, std::vector<std::vector<long long>>(
-                 m + 1, std::vector<long long>(k + 1, kInf)));
+      s.size() + 1, std::vector<std::vector<long long>>(
+                        t.size() + 1, std::vector<long long>(k + 1, kInf)));
+  DpBuild(dp, s, t, k);
+  long long ans = kInf;
+  for (long long i = 0; i <= k; ++i) {
+    ans = std::min(ans, std::max(dp[s.size()][t.size()][i] - k, 0LL));
+  }
+  if (ans < kInf / 2) {
+    std::cout << ans << '\n';
+  } else {
+    std::cout << -1 << '\n';
+  }
+  return 0;
+}
+
+void DpBuild(std::vector<std::vector<std::vector<long long>>>& dp,
+             const std::string& s, const std::string& t, long long k) {
+  auto n = static_cast<long long>(s.size()),
+       m = static_cast<long long>(t.size());
   dp[0][0][0] = 0;
   for (long long i = 0; i <= k; ++i) {
     if (i >= n) {
@@ -47,14 +64,4 @@ int main() {
       }
     }
   }
-  long long ans = kInf;
-  for (long long i = 0; i <= k; ++i) {
-    ans = std::min(ans, std::max(dp[n][m][i] - k, 0LL));
-  }
-  if (ans < kInf / 2) {
-    std::cout << ans << '\n';
-  } else {
-    std::cout << -1 << '\n';
-  }
-  return 0;
 }
