f02b47c218cb11326abede6805d7f7e516a62f11
Bakytw <89724301+Bakytw@users.noreply.github.com>
2023-11-17 17:34:34 +0300

    Update taskC.cpp

--- 10contest/taskC.cpp
+++ 10contest/taskC.cpp
@@ -1,39 +1,35 @@
 // no_concepts
 #include <iostream>
-#include <map>
 #include <queue>
-#include <set>
 #include <vector>
 
-const static int kSigma = 26;
-
-struct Node {
-  std::vector<int> to;
-  std::vector<int> indices;
-  int size{0};
-  int link{-1};
-  int up{-1};
-  bool term{false};
-  Node() { to.assign(kSigma, -1); };
-};
-
 class Trie {
+  const static int kSigma = 26;
+  struct Node {
+    std::vector<int> to;
+    std::vector<int> indices;
+    int size{0};
+    int link{-1};
+    int up{-1};
+    bool term{false};
+    Node() { to.assign(kSigma, -1); };
+  };
+  std::vector<Node> t_;
+
  public:
-  void Add(const std::string& s, int i) {
-    if (t_.empty()) {
-      t_.emplace_back();
-    }
-    int v = 0;
-    for (auto c : s) {
-      if (t_[v].to[c - 'a'] == -1) {
+  Trie() { t_.emplace_back(); }
+  void Add(const std::string& str, int index) {
+    int vertex = 0;
+    for (auto c : str) {
+      if (t_[vertex].to[c - 'a'] == -1) {
         t_.emplace_back();
-        t_[v].to[c - 'a'] = static_cast<int>(t_.size()) - 1;
+        t_[vertex].to[c - 'a'] = static_cast<int>(t_.size()) - 1;
       }
-      v = t_[v].to[c - 'a'];
+      vertex = t_[vertex].to[c - 'a'];
     }
-    t_[v].indices.push_back(i);
-    t_[v].size = static_cast<int>(s.size());
-    t_[v].term = true;
+    t_[vertex].indices.push_back(index);
+    t_[vertex].size = static_cast<int>(str.size());
+    t_[vertex].term = true;
   }
 
   void AhoCorasik() {
@@ -43,28 +39,33 @@ class Trie {
         t_[0].to[i] = 0;
       }
     }
-    std::queue<int> q;
-    q.push(0);
-    while (!q.empty()) {
-      int v = q.front();
-      q.pop();
+
+    std::queue<int> queue;
+    queue.push(0);
+    while (!queue.empty()) {
+      int v = queue.front();
+      queue.pop();
+
       for (int c = 0; c < kSigma; ++c) {
         int u = t_[v].to[c];
         if (t_[u].link != -1) {
           continue;
         }
         t_[u].link = v == 0 ? 0 : t_[t_[v].link].to[c];
+
         if (t_[t_[u].link].term) {
           t_[u].up = t_[u].link;
         } else {
           t_[u].up = t_[t_[u].link].up;
         }
+
         for (int d = 0; d < kSigma; ++d) {
           if (t_[u].to[d] == -1) {
             t_[u].to[d] = t_[t_[u].link].to[d];
           }
         }
-        q.push(u);
+
+        queue.push(u);
       }
     }
   }
@@ -80,22 +81,20 @@ class Trie {
   int GetVertex(int u, int c) const { return t_[u].to[c]; }
 
   std::vector<int> GetIndices(int u) const { return t_[u].indices; }
-
- private:
-  std::vector<Node> t_;
 };
 
-std::vector<std::vector<int>> Entries(const Trie& trie, int n,
-                                      const std::string& s) {
+std::vector<std::vector<size_t>> Entries(const Trie& trie, int num,
+                                         const std::string& str) {
   int v = 0;
-  std::vector<std::vector<int>> entries(n);
-  for (int i = 0; i < static_cast<int>(s.size()); ++i) {
-    v = trie.GetVertex(v, s[i] - 'a');
+  std::vector<std::vector<size_t>> entries(num);
+  for (size_t i = 0; i < str.size(); ++i) {
+    v = trie.GetVertex(v, str[i] - 'a');
     if (trie.GetTerm(v)) {
       for (auto u : trie.GetIndices(v)) {
         entries[u].push_back(i + 1 - trie.GetSize(v));
       }
     }
+
     int u = trie.GetUp(v);
     while (u != -1) {
       for (auto k : trie.GetIndices(u)) {
@@ -107,13 +106,13 @@ std::vector<std::vector<int>> Entries(const Trie& trie, int n,
   return entries;
 }
 
-void Output(const std::vector<std::vector<int>>& entries, int n) {
-  for (int i = 0; i < n; ++i) {
+void Output(const std::vector<std::vector<size_t>>& entries, int num) {
+  for (int i = 0; i < num; ++i) {
     std::cout << entries[i].size() << ' ';
     for (auto index : entries[i]) {
       std::cout << index + 1 << ' ';
     }
-    std::cout << std::endl;
+    std::cout << '\n';
   }
 }
 
@@ -121,18 +120,20 @@ int main() {
   std::ios_base::sync_with_stdio(false);
   std::cin.tie(nullptr);
   std::cout.tie(nullptr);
-  std::string s;
-  int n;
+
+  std::string str;
+  int num;
   Trie trie;
-  std::cin >> s;
-  std::cin >> n;
-  for (int i = 0; i < n; ++i) {
-    std::string p;
-    std::cin >> p;
-    trie.Add(p, i);
+  std::cin >> str;
+  std::cin >> num;
+  for (int i = 0; i < num; ++i) {
+    std::string word;
+    std::cin >> word;
+    trie.Add(word, i);
   }
+
   trie.AhoCorasik();
-  std::vector<std::vector<int>> entries(Entries(trie, n, s));
-  Output(entries, n);
+  std::vector<std::vector<size_t>> entries(Entries(trie, num, str));
+  Output(entries, num);
   return 0;
 }
